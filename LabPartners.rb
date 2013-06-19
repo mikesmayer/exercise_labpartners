e9b52d839130393597454526f74ca121bf8a5617
Mike Mayer <mike@industrygraph.com>
2013-06-19 17:30:14 -0400

    with solution

---
+++ LabPartners.rb
@@ -0,0 +1,26 @@
+puts "Hi, let me help you create groups for your class"
+
+puts "How many students should be in each group?"
+
+# groupsize = gets.chomp
+
+groupsize = 3
+
+students = ["Alan Appelstein", "Alice Mottola", "Alina Guzman", "andrew silbersmith", "Anuj Jhunjhunwala", "Ben Israeli",
+  "Beryl Schragger", "Chris Clearfield", "Christine Coulter", "Christopher Huyett", "Christopher Wright", "Corbin Sykes",
+  "Eleyna Whittingham", "Eric Gong", "Jamal Powell", "Jason Darcy", "Kevon Cheung", "Mike Mayer", "Mike Vanger", "Nick Lombardi",
+  "Phillip Lamplugh", "Teddy Cleveland", "Thomas Yu", "Tim Blonski", "Tom Brennan", "Tricia Dougals", "Wesley Carr", "Zachariah Reitano"]
+
+# puts students.sort_by{rand}.each_slice(groupsize).to_a.inspect
+
+subgroups = students.sort_by{rand}.each_slice(groupsize).to_a
+
+if students.length % groupsize != 0
+  lizard = subgroups[-2].concat(subgroups[-1])
+else print subgroups
+end
+
+subgroups.pop(2)
+subgroups.push(lizard)
+
+print subgroups
